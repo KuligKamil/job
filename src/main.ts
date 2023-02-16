84035e6736decfae46464c1ec31909f0a1669cfc
KuligKamil <kamil.marek.kulig@gmail.com>
2023-02-17 00:58:20 +0100

    refactor: extract vuetify, for more readable imports

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,6 @@
-import "vuetify/styles";
-import "@mdi/font/css/materialdesignicons.css";
-import { createVuetify, type ThemeDefinition } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
-
-const myCustomLightTheme: ThemeDefinition = {
-  dark: false,
-  colors: {
-    // background: '#F0FAFB',
-    background: "#effafa",
-    // surface: '#FFFFFF',
-    // primary: '#64B9BC',
-    // 'primary-darken-1': '#3700B3',
-    // secondary: '#03DAC6',
-    // 'secondary-darken-1': '#018786',
-    // error: '#B00020',
-    // info: '#2196F3',
-    // success: '#4CAF50',
-    // warning: '#FB8C00',
-  },
-};
-
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    defaultTheme: "myCustomLightTheme",
-    themes: {
-      myCustomLightTheme,
-    },
-  },
-});
+import { vuetify } from "./vuetify";
 
 createApp(App).use(createPinia()).use(vuetify).mount("#app");
